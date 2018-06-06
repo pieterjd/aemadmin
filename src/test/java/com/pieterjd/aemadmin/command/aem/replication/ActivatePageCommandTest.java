7cc1f89b51cfef1873a0955563aa1f93d7c00d59
Pieter-Jan Drouillon <drouillon@hotmail.com>
2018-06-06 09:18:02 +0200

    Added unittest for ActivatePageCommand

---
+++ src/test/java/com/pieterjd/aemadmin/command/aem/replication/ActivatePageCommandTest.java
@@ -0,0 +1,34 @@
+package com.pieterjd.aemadmin.command.aem.replication;
+
+import com.pieterjd.aemadmin.command.crx.node.DeleteNodeCommand;
+import com.pieterjd.aemadmin.command.crx.property.GetPropertyCommand;
+import com.pieterjd.aemadmin.config.LocalPublishConfigBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActivatePageCommandTest {
+    private String path;
+    @Before
+    public void setUp() throws Exception {
+        path = "/content/we-retail";
+    }
+
+    @Test
+    public void activate(){
+        //remove page on publish
+        DeleteNodeCommand dnc = new DeleteNodeCommand(path);
+        dnc.setConfigBuilder(new LocalPublishConfigBuilder());
+        dnc.execute();
+        ActivatePageCommand apc = new ActivatePageCommand(path);
+        apc.execute();
+        assertTrue(apc.isSuccess());
+        //check on local publish
+        GetPropertyCommand gpc = new GetPropertyCommand(path,"jcr:primaryType");
+        gpc.setConfigBuilder(new LocalPublishConfigBuilder());
+        gpc.execute();
+        assertTrue(gpc.isSuccess());
+    }
+
+}
