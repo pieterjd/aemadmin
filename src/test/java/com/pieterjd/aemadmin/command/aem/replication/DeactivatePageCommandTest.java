fd71181e8f14cd68aef3990708982aa1199280bd
Pieter-Jan Drouillon <drouillon@hotmail.com>
2018-06-06 09:25:04 +0200

    Added unittest for DectivatePageCommand

---
+++ src/test/java/com/pieterjd/aemadmin/command/aem/replication/DeactivatePageCommandTest.java
@@ -0,0 +1,33 @@
+package com.pieterjd.aemadmin.command.aem.replication;
+
+import com.pieterjd.aemadmin.command.crx.property.GetPropertyCommand;
+import com.pieterjd.aemadmin.config.LocalPublishConfigBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeactivatePageCommandTest {
+    private String path;
+    @Before
+    public void setUp() throws Exception {
+        path = "/content/we-retail";
+    }
+
+    @Test
+    public void deactivate(){
+        //check page is on publish
+        //check on local publish
+        GetPropertyCommand gpc = new GetPropertyCommand(path,"jcr:primaryType");
+        gpc.setConfigBuilder(new LocalPublishConfigBuilder());
+        gpc.execute();
+        assertTrue(gpc.isSuccess());
+
+        DeactivatePageCommand dpc = new DeactivatePageCommand(path);
+        dpc.execute();
+        //recheck if it's gone
+        gpc.execute();
+        assertFalse(gpc.isSuccess());
+    }
+
+}
