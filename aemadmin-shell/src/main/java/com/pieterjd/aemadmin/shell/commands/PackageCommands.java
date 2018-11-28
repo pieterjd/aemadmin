1cbbc0bb02d29da25dcd6e5d1ad445c66865d774
Pieter-Jan Drouillon <drouillon@hotmail.com>
2018-11-28 19:14:12 +0100

    Package list now uses table

--- aemadmin-shell/src/main/java/com/pieterjd/aemadmin/shell/commands/PackageCommands.java
+++ aemadmin-shell/src/main/java/com/pieterjd/aemadmin/shell/commands/PackageCommands.java
@@ -1,25 +1,44 @@
 package com.pieterjd.aemadmin.shell.commands;
 
+import com.github.tsohr.JSONArray;
+import com.github.tsohr.JSONObject;
 import com.pieterjd.aemadmin.command.packmgr.BuildPackageCommand;
 import com.pieterjd.aemadmin.command.packmgr.DownloadPackageCommand;
 import com.pieterjd.aemadmin.command.packmgr.InstallPackageCommand;
 import com.pieterjd.aemadmin.command.packmgr.ListPackagesCommand;
 import com.pieterjd.aemadmin.shell.utils.DirectoryValueProvider;
-import org.springframework.shell.standard.FileValueProvider;
+import com.pieterjd.aemadmin.shell.utils.JsonTableModel;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+import org.springframework.shell.table.BorderStyle;
+import org.springframework.shell.table.Table;
+import org.springframework.shell.table.TableBuilder;
+import org.springframework.shell.table.TableModel;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @ShellComponent
 public class PackageCommands {
     @ShellMethod("List all packages")
-    public String listPackages() throws IOException {
+    public Table listPackages() throws IOException {
         ListPackagesCommand c = new ListPackagesCommand();
         c.execute();
-        return c.getHttpResponseAsJSON().toString(1);
+
+        List<JSONObject> packages = new ArrayList<>();
+        JSONArray data = c.getHttpResponseAsJSON()
+                .getJSONObject("crx").getJSONObject("response")
+                .getJSONObject("data").getJSONObject("packages").getJSONArray("package");
+        for(int i=0;i<data.length();i++){
+            packages.add(data.getJSONObject(i));
+        }
+        TableModel model = new JsonTableModel(packages);
+        return new TableBuilder(model)
+                .addFullBorder(BorderStyle.air)
+                .build();
     }
 
     @ShellMethod("Installs a package")
